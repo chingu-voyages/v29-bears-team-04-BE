c8fd8eead38675e6db1b8b481ed7d0b717975e97
changangus <63685366+changangus@users.noreply.github.com>
2021-05-04 19:48:49 -0400

    set httpOnly to false

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ const main = async() => {
       }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years,
-        // httpOnly: true,
+        httpOnly: false
         // sameSite: 'lax',
         // secure: __prod__,
         // domain: __prod__ ? ".herokuapp.com" : undefined,
