0159747132e7a795119f1b069585d751729f3aa6
changangus <63685366+changangus@users.noreply.github.com>
2021-05-07 13:39:43 -0400

    origin is either local host or deployed origin

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const main = async() => {
   // Cors Config:
   app.use(
     cors({
-      origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+      origin: process.env.CORS_ORIGIN as string | 'http://localhost:3000',
       credentials: true,
     })
   );
