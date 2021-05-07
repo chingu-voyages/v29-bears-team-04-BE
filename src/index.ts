9958b8709cb27c8296ee3b241eb87b1178205a21
changangus <63685366+changangus@users.noreply.github.com>
2021-05-07 13:38:25 -0400

    cors origin in env variable and added localhost as origin if it is undefined

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const main = async() => {
   // Cors Config:
   app.use(
     cors({
-      origin: 'https://resplashed.herokuapp.com' || 'http://localhost:3000',
+      origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
       credentials: true,
     })
   );
